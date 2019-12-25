39dcb06e1b4c39b021e1ab0468b5620afc1f6101
Andréa Alencar <andreaflether@gmail.com>
2019-12-25 12:00:27 -0300

    add hero when user is not logged in, add css and new notification style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :shots do
     resources :comments
+    member do
+      put 'like', to: "shots#like"
+      put 'unlike', to: "shots#unlike"
+    end
   end
   devise_for :users, controllers: {registrations: 'registrations'}
-  root 'home#index'
+  root 'shots#index'
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
